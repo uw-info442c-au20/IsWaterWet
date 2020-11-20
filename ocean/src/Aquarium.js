9ab5c2d26831e138c6129b44135c6f6c4b0cd244
Connor Voelk <voelkc@uw.edu>
2020-11-20 15:42:34 -0800

    Menu stuff

--- ocean/src/Aquarium.js
+++ ocean/src/Aquarium.js
@@ -4,8 +4,19 @@ class Aquarium extends Component {
     render() {
         return (
              <div className="aquarium-background">
+                 <MenuController/>
              </div>
         );
+    }
+}
+
+class MenuController extends Component{
+    render(){
+        return(
+            <div class="menu-controller">
+                <h1>AQUARIUM     EVENTS</h1>
+            </div>
+        )
     }
 }
 
