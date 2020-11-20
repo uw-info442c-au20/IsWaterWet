a3232d509da11e7cb5c0d48847378ff57470b445
Connor Voelk <voelkc@uw.edu>
2020-11-20 15:24:16 -0800

    New background

--- ocean/src/App.js
+++ ocean/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './index.css';
 import Aquarium from './Aquarium';
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
       <Aquarium/>
+      <Menu/>
     </div>
   );
 }
--- ocean/src/Menu.js
+++ ocean/src/Menu.js
@@ -3,11 +3,30 @@ import React, { Component } from 'react';
 class Menu extends Component{
     render(){
         return(
-            <div>
-
+            <div className = "menu">
+                <FishMenu/>
             </div>
         )
     }
 }
 
+
+class FishMenu extends Component{
+    render(){
+        return(
+            <div className = "fish-menu">
+                <img src="img/Real-Clownfish.png" />
+                <img src="img/Real-Jellyfish.png" />
+                <img src="img/Real-Cuttlefish.png" />
+                <img src="img/Real-Yellowtang.png" />
+            </div>
+        )
+    }
+}
+
+class EventMenu extends Component{
+
+}
+
 export default Menu;
+
