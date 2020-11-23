4a987790c5b69ab8d8cd602fc79a21abca189d9e
Connor Voelk <voelkc@uw.edu>
2020-11-22 18:40:22 -0800

    Installed relevant library for animation

--- ocean/src/Aquarium.js
+++ ocean/src/Aquarium.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'; 
+import TweenOne from 'rc-tween-one';
+import PropTypes from 'prop-types';
 
 class Aquarium extends Component {
     render() {
         return (
              <div className="aquarium-background">
+                 <Fish/>
                  <MenuController/>
              </div>
         );
     }
+}
+
+
+class Fish extends Component{
+    render(){
+        return(
+            <div className= "fish">
+                {this.fishImg("img/Cartoon-Clownfish.png")}              
+            </div>
+        )
+    }
+
+    fishImg(imgPath){
+        return (<img src={imgPath} alt="fish"/>)
+    }
+
+    movement(){
+        
+    }
+
+
 }
 
 class MenuController extends Component{
